f87ff46a1e3f5f25472bf1024573aff1d9330d6f
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2019-10-12 18:34:43 -0400

    get year links from a team page

--- src/parser.py
+++ src/parser.py
@@ -24,3 +24,22 @@ def get_team_links(link, links_q):
         if full_link not in results:
             links_q.put(full_link)
             results.add(full_link)
+
+
+def get_team_years(link, years_q):
+    import requests
+    from bs4 import BeautifulSoup
+
+    page = requests.get(link)
+    if page.status_code != 200:
+        print('Failed requesting page {}!'.format(link))
+        page.raise_for_status()
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    section = soup.find('table', id='franchise_years')
+
+    for hyperlink in section.find_all('a'):
+        full_link = 'http://www.baseballreference.com' + hyperlink.get('href')
+        years_q.put(full_link)
+    
