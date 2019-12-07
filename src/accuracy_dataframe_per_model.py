94969feb42730ba61dc6a8375a8a82b4c60ac590
nlgriffin <nlg297@nyu.edu>
2019-12-07 15:49:08 -0500

    Updating most recent script with commentary

--- src/accuracy_dataframe_per_model.py
+++ src/accuracy_dataframe_per_model.py
@@ -5,6 +5,10 @@ Created on Sat Dec  7 15:40:23 2019
 @author: ngrif
 """
 def check_accuracy_data(threshold, prediction, actual):
+    '''
+    This function will take two series of equal length and only calculate 
+    accuracy for predictions that meet the threshold criteria. 
+    '''
     accuracies = []
     my_list = []
     for preds, truth in zip(prediction, actual):
@@ -22,6 +26,14 @@ def check_accuracy_data(threshold, prediction, actual):
         subsample_percent = len(accuracies)/len(prediction)
     return [threshold, accuracy, subsample_percent]
 
+
+'''
+For the below, update predictions list to be your list of predict probas in
+15,30,60,90 order and same for tests list being actual outcomes. 
+Change 'your_model_type' (2 serperate inputs) below to some 2-4 character string
+that describes your model. (RF, NN, GB, LR, etc etc)
+Please push csv to git once complete.
+'''
 thresholds = [.52, .54, .55, .56]
 predictions = [pred_15, pred_30, pred_60, pred_90]
 tests = [y_test_15,y_test_30,y_test_60,y_test_90]
@@ -38,4 +50,4 @@ df.columns = ['Threshold', 'Accuracy', 'Percent_of_Samples', 'Dataset', 'Model']
 os.chdir(r"..\data")
 
 # Save file with your model name 
-df.to_csv('your_file_name')
+df.to_csv('accuracy_dataframe_your_model_type')
